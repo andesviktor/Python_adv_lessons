32f9cb510c8bbb94e7bf7585265819f154a214f5
Viktor Andes <viktorandes42@gmail.com>
2021-02-09 22:28:45 +0200

    Add SOLID and refactor

---
+++ lessons/lesson_4_oop/main.py
@@ -0,0 +1,16 @@
+from collections import UserDict
+
+# class A(dict):
+#     def __init__(self):
+#         super(A,self).__init__()
+#
+#         self['books'] = {1: "book_1", 2: "book_2"}
+#
+# a = A()
+#
+# print(a)
+# print(a.get('qwertyu'))
+# print(a.setdefault('qweq'))
+# print(a)
+
+# Note: ООП
